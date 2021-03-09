d42af2dc12e5fa59e4c398bae5a0c8a918833ca9
BETHANY FOLINO <bethany.folino@gmail.com>
2021-03-08 20:27:25 -0800

    Added react-router-dom (it was missing) and Link tags and Route tags, and tried to make state variables for user input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import "./App.css";
 import { useState } from "react";
 import todosList from "./todos.json";
+import { Route } from "react-router-dom";
 
-import TodoList from "./components/TodoList"
-import Footer from "./components/Footer"
+import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
 
 function App() {
   const [todos, setTodos] = useState(todosList);
+  const [userInputs, setUserInputs] = useState(todosList);
 
   return (
     <section className="todoapp">
       <header className="header">
         <h1>todos</h1>
         <input
+          // onChange={function that updates user's input in state}
           className="new-todo"
           placeholder="What needs to be done?"
           autofocus
         />
       </header>
       <TodoList todos={todos} />
-      <Footer/>
+      <Footer />
+      <Route path="/active"></Route>
+      <Route path="/completed"></Route>
+      <Route path="/"></Route>
     </section>
   );
 }
